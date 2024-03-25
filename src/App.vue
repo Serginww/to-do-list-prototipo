fcc658e5817303a05c1b76a79152d9f04778cee0
sergio pinheiro <sergiopinheiro_2002@outlook.com>
2024-03-25 12:44:11 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container grid-xs py-2">
+      <img class="img-responsive" src="@/assets/lista.png" alt="coffetagrtodo">
+      <form @submit.prevent="addTarefa(tarefa)">
+        <div class="input-group">
+          <input type="text" v-model="tarefa.description" class="form-input" placeholder="Adicionar uma tarefa">
+          <button class="btn btn-success input-group-btn"><i class="icon icon-arrow-right"></i>Adicionar</button>
+        </div>
+      </form>
+      <ul class="step">
+        <li class="step-item active">
+          <a href="#" class="tooltip" data-tooltip="Step 1">Step 1</a>
+        </li>
+        <li class="step-item active">
+          <a href="#" class="tooltip" data-tooltip="Step 2">Step 2</a>
+        </li>
+        <li class="step-item">
+          <a href="#" class="tooltip" data-tooltip="Step 3">Step 3</a>
+        </li>
+        <li class="step-item">
+          <a href="#" class="tooltip" data-tooltip="Step 4">Step 4</a>
+        </li>
+      </ul>
+    </div>
+    <div class="tarefa-list">
+      <tarefa v-for="t in tarefas" :key="t.id" @toggle ="toggleTarefa" @remove = "removeTarefa" :tarefa="t"></tarefa>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tarefa from './components/Tarefa'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Tarefa
+  },
+  data() {
+    return {
+      tarefas: [],
+      tarefa: { description: '', checked: false }
+    };
+  },
+  methods: {
+    addTarefa(tarefa) {
+      tarefa.id = Date.now();
+      this.tarefas.push(tarefa);
+      this.tarefa = { description: '', checked: false }; 
+    },
+
+      toggleTarefa(tarefa){
+        const index = this.tarefas.findIndex(item => item.id === tarefa.id)
+        if (index > -1){
+          const checked = !this.tarefas[index].checked;
+          this.tarefas[index].checked = checked;
+        }
+      },
+
+      removeTarefa(tarefa){
+        const index = this.tarefas.findIndex(item => item.id === tarefa.id)
+          if (index > -1){
+            this.tarefas.splice(index, 1);
+          }
+        }
+
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+img {
+  max-width: 150px;
+  margin: 0 auto;
+}
+.tarefa-list{
+padding-top: 5rem;
+
+
 }
 </style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="tile flex-centered" :class="{checked: tarefa.checked}">
+        <dir class="tile-content">
+            <div class="title-subtitle">{{ tarefa.description }}
+                <div>{{ tarefa.checked }}</div>
+        </div>
+        <div class="tile-action">
+            <button @click = "$emit('toggle', tarefa)" class="btn btn-primary"> 
+            <span v-if="tarefa.checked">Desmarcar</span>
+            <span v-else> Conlcuido</span>
+
+            </button>
+
+            <button @click ="$emit('remove', tarefa)" class="btn btn-error"> Remover </button>
+        </div>
+        </dir>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ListaDeTarefas",
+    
+
+    props: {
+
+        tarefa: {type: Object,required: true}
+    }
+};
+</script>
+
+<style scoped>
+ .checked{
+    text-decoration: line-through ;
+    color: lightslategray;
+ }
+</style>
